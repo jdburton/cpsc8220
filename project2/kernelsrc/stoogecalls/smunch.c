81183d6b35c3b495883c12783ea84915c8f836e4
James Burton <james.daly.burton@gmail.com>
2016-04-04 21:39:38 -0400

    Clean up smunch.c

--- project2/kernelsrc/stoogecalls/smunch.c
+++ project2/kernelsrc/stoogecalls/smunch.c
@@ -1,14 +1,24 @@
+/*
+ * smunch.c
+ *
+ * CPSC 8220
+ * April 5, 2016
+ *
+ * James Burton, Jas Eckard
+ *
+ * smunch - Send a barrage of signals, and mean it!
+ *
+ */
+
 #include <linux/linkage.h>
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
-#include <linux/delay.h>
 
 #define SMUNCH_KILL_BIT ( 1UL << (SIGKILL - 1))
 
 SYSCALL_DEFINE2(smunch, pid_t, pid, unsigned long, bit_pattern)
 {
-
 	int ret = -1;		// assume failure
 	struct pid *pid_s;
 	struct task_struct *t;
@@ -20,40 +30,32 @@ SYSCALL_DEFINE2(smunch, pid_t, pid, unsigned long, bit_pattern)
 	// get the pid from the pid_t
 	rcu_read_lock();
 	pid_s = find_vpid(pid);
-	rcu_read_unlock();
-	if (!pid_s)
+	if (!pid_s){
+	    rcu_read_unlock();
 		goto exit;
-
+    }
 	// get the task_struct from the pid
-	rcu_read_lock();
 	t = pid_task(pid_s, PIDTYPE_PID);
 	rcu_read_unlock();
 	if (!t)
 		goto exit;
 
 	if (bit_pattern & SMUNCH_KILL_BIT) {
-		bit_pattern = SMUNCH_KILL_BIT;	// only send kill if it is only bit set.
+        // only send SIGKILL if kill bit is set.
+		bit_pattern = SMUNCH_KILL_BIT;	
 	}
 	// hunt for zombies.
 	if (t->exit_state & EXIT_ZOMBIE) {
-		printk(KERN_ALERT "pid %d is a zombie!\n", pid);
-		// only clean up on a kill
+		// only clean up on SIGKILL, otherwise ignore.
 		if (bit_pattern == SMUNCH_KILL_BIT) {
 			// cleanup after the zombie
-			printk(KERN_ALERT
-			       "Killing zombie %d with your wooden sword!\n",
-			       pid);
-
-			msleep(20);
 			release_task(t);
 		}
 		ret = 0;	// success!
 		goto exit;
-
 	}
-	printk(KERN_ALERT "pid %d is not a zombie. Whew!\n", pid);
-
-	lock_task_sighand(t, &flags);
+	
+    lock_task_sighand(t, &flags);
 
 	// set the task_state to TASK_INTERRUPTABLE
 	set_task_state(t, TASK_INTERRUPTIBLE);
